cfe8ddd96044f0b7f9d052c69d856bdde402c2cc
LiveSquid <liamashok@gmail.com>
2025-04-24 13:15:21 -0400

    Test

--- app/_layout.js
+++ app/_layout.js
@@ -3,7 +3,7 @@ import './global.css'
 import { StatusBar } from 'expo-status-bar';
 import { SafeAreaView, StyleSheet, Text, View } from 'react-native';
 
-export default function RootLayout() {
+export default function App() {
   return (
     <>
       <SafeAreaView className = 'flex-1 justify-center items-center bg-green-100'>
